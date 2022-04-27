7ac99e182218199e8a691849b967599d2a4f7e99
Camilo Arriagada Vallejos <95378920+c4miloarriagada@users.noreply.github.com>
2022-04-26 21:08:28 -0400

    backfinished

--- api/src/routes/activity.js
+++ api/src/routes/activity.js
@@ -1,85 +1,59 @@
 const express = require('express');
 const { Country, Activity } = require('../db')
-const Model= require('../models/Activity')
+const Model = require('../models/Activity')
 const router = express.Router()
 
+router.post('/', async (req, res) => {
 
 
-
-
-
-
-
-
-router.post('/', async(req, res)=>{
- /*
-    const {name, difficult, duration, season, country} = req.body
-   const seasons = ['summer', 'autumn', 'spring', 'winter'] 
-   const level = ['1','2','3','4','5']
-  
+    const { name, difficult, duration  } = req.body;
+    const season = req.body.season.toLowerCase();
+    const country = req.body.country[0].toUpperCase().concat(req.body.country.slice(1).toLowerCase());
+    const seasons = ['summer', 'autumn', 'spring', 'winter'];
+    const level = ['1', '2', '3', '4', '5'];
    
-   if(name === '' || typeof name !== 'string') return res.status(404).send('Must include a name')
-   if(difficult === '' || (!level.includes(difficult))) return res.status(404).send('Must include a valid difficult. min: 1 max: 5')
-   if(season === '' || (!seasons.includes(season))) return res.status(404).send('Must include a valid season. summer - autumn - spring -winter')
-   if(duration === '') return res.status(404).send('Must include a duration') 
-   if(country === '') return res.status(404).send('Must include a valid Country')
+    if (name === '' || typeof name !== 'string') return res.status(404).send('Must include a name');
+    if (difficult === '' || (!level.includes(difficult))) return res.status(404).send('Must include a valid difficult. min: 1 max: 5') ;
+    if (season === '' || (!seasons.includes(season))) return res.status(404).send('Must include a valid season. Summer - Autumn - Spring -Winter');
+    if (duration === '') return res.status(404).send('Must include a duration');
+    if (country === '') return res.status(404).send('Must include a valid Country');
 
-   
-   const findActivityExist = await Activity.findAll({
-     where:{
-         name,
-         difficult,
-         duration,
-         season
-     }
-       
-   })
-
-   
-   if(findActivityExist === null){
-       const create = await Activity.create({
-           name,
-           difficult,
-           duration,
-           season
-        })
-        country.map(e=>{
-            Country.findAll({
-                where:{
-                    id:{
-                        [Op.eq] : e
-                    }
-                }
-            })
-            create.addCountry(country)
-        })
-        res.json(country)
-    }
-
-*/
-
-    const {name, difficult, duration, season, country} = req.body
-    const seasons = ['summer', 'autumn', 'spring', 'winter'] 
-    const level = ['1','2','3','4','5']
-   
-    
-    if(name === '' || typeof name !== 'string') return res.status(404).send('Must include a name')
-    if(difficult === '' || (!level.includes(difficult))) return res.status(404).send('Must include a valid difficult. min: 1 max: 5')
-    if(season === '' || (!seasons.includes(season))) return res.status(404).send('Must include a valid season. summer - autumn - spring -winter')
-    if(duration === '') return res.status(404).send('Must include a duration') 
-    if(country === '') return res.status(404).send('Must include a valid Country')
- 
-    
     try {
-        const createActivity = await Activity.create(req.body)
-        res.status(201).json(createActivity)
+
+        const findActivity = await Activity.findOne({
+            where: {
+                name,
+                difficult,
+                duration,
+                season,
+                  
+            },
+            include: Country
+           
+        })
+ 
+        const findCountry = await Country.findOne({
+            where:{
+                name: country
+            }
+        })
+
+        if(!findCountry)  res.send('Country doesnt exist')
+        if (!findActivity || country != country) {
+
+            const createActivity = await Activity.create({name, difficult, duration,  country, season})
+            createActivity.addCountry(findCountry)
+            res.status(201).json(createActivity)
+            
+        } else {
+            return res.status(400).send('The Activity Was Created')
+        }
     } catch (error) {
-        res.status(404).send('error')
+        res.status(404).send('error', error)
     }
 
 
+});
 
-})
 
-
-module.exports  = router;
+module.exports = router;
--- api/src/routes/country.js
+++ api/src/routes/country.js
@@ -5,52 +5,52 @@ const router = express.Router()
 
 
 
-router.get('/', async(req, res)=>{
-  
-const {name} = req.query
-try {
-    
-    if(name){
+router.get('/', async (req, res) => {
+
+  const { name } = req.query
+  try {
+
+    if (name) {
       const queryCountry = await Country.findAll({
-        where:{
-          name:{
-           [Op.iLike] : `%${name}%` 
+        where: {
+          name: {
+            [Op.iLike]: `%${name}%`
           }
         },
-        include: Activity 
+        include: Activity
       })
-      if(queryCountry.length === 0){
+      if (queryCountry.length === 0) {
 
         return res.status(404).send('Country Doesnt Exists')
       }
       return res.json(queryCountry)
-    }else{
+    } else {
       const listCountry = await Country.findAll({
-        order:[
-         ['name', 'ASC']
+        order: [
+          ['name', 'ASC']
         ],
-        attributes:[
-          'id','name', 'img','subregion', 'population', 'continent'
+        attributes: [
+          'id', 'name', 'img', 'subregion', 'population', 'continent'
         ],
-       })
-       return  res.send(listCountry)
+      })
+      return res.send(listCountry)
     }
   } catch (error) {
-    res.status(500).send('ERROR',error)
+    res.status(500).send('ERROR', error)
   }
 });
 
 
-router.get('/:id', async(req, res)=>{
+router.get('/:id', async (req, res) => {
   const idCountry = req.params.id.toUpperCase()
-  
+
   try {
-    const searchCountry = await Country.findByPk(idCountry,{
+    const searchCountry = await Country.findByPk(idCountry, {
       include: Activity
     })
-    searchCountry 
-    ? res.json(searchCountry) 
-    : res.status(404).send(`${idCountry} Doesnt Exist`)
+    searchCountry
+      ? res.json(searchCountry)
+      : res.status(404).send(`${idCountry} Doesnt Exist`)
 
 
   } catch (error) {
